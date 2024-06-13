059a0304077e82831249a696f3e5fe802b1b4b86
김종경 <111286262+JONG-KYEONG@users.noreply.github.com>
2024-06-13 16:34:39 +0900

    Add files via upload

--- mediaPipe/main.py
+++ mediaPipe/main.py
@@ -19,12 +19,12 @@ gesture = [[True, True, True, True, True, "Hi!"],
 while True:
     success,img = cap.read()
     h,w,c = img.shape
-    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    imgRGB = cv2.cvtColor(img,cv2.COLOR_BGR2RGB) 
     results = my_hands.process(imgRGB)
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             for i in range(0,5):
-                open[i] = dist(handLms.landmark[0].x,handLms.landmark[0].y, handLms.landmark[compareIndex[i][0]].x, handLms.>
+                open[i] = dist(handLms.landmark[0].x,handLms.landmark[0].y, handLms.landmark[compareIndex[i][0]].x, handLms.landmark [compareIndex[i][0]].y) < dist(handLms.landmark[0].x,handLms. landmark[0].y,handLms.landmark [compareIndex[i][1]].x, handLms.landmark [compareIndex[i][1]].y)
             print(open)
             text_x = (handLms.landmark[0].x * w)
             text_y= (handLms.landmark[0].y *h)
@@ -33,7 +33,8 @@ while True:
                 for j in range(0,5):
                     if(gesture[i][j] != open[j]): flag = False
                 if(flag == True):
-                    cv2.putText(img, gesture[i][5], (round(text_x)-50, round(text_y) - 250),cv2.FONT_HERSHEY_PLAIN,4,(0,0,0)>
+                    cv2.putText(img, gesture[i][5], (round(text_x)-50, round(text_y) - 250),cv2.FONT_HERSHEY_PLAIN,4,(0,0,0),4)
             mpDraw.draw_landmarks (img, handLms, mphands. HAND_CONNECTIONS)
     cv2.imshow("HandTracking", img)
+    cv2.waitKey(1)
 
