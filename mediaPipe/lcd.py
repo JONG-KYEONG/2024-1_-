1b4445b781a8037c63d6d07d7a8579eaae440f6c
JongKyeong Kim <oo7bangjk@naver.com>
2024-06-18 16:38:12 +0900

    .

--- mediaPipe/lcd.py
+++ mediaPipe/lcd.py
@@ -1,8 +1,42 @@
-import RPi.GPIO as GPIO
-from RPLCD.gpio import CharLCD
-import time
+# import RPi.GPIO as GPIO
+# from RPLCD.gpio import CharLCD
+# import time
 
-# Define the pin configuration
+# # Define the pin configuration
+# LCD_RS = 11
+# LCD_E = 10
+# LCD_D4 = 6
+# LCD_D5 = 5
+# LCD_D6 = 4
+# LCD_D7 = 1
+
+# # Setup GPIO mode
+# GPIO.setmode(GPIO.BCM)
+
+
+# # Initialize the LCD
+# lcd = CharLCD(
+#     numbering_mode=GPIO.BCM,
+#     cols=16, rows=2,
+#     pin_rs=LCD_RS, pin_e=LCD_E,
+#     pins_data=[LCD_D4, LCD_D5, LCD_D6, LCD_D7]
+# )
+
+# # Display text on the LCD
+# lcd.write_string('HELLO WORLD')
+
+# # Wait for user input
+# input("Press any key to continue...")
+
+# # Clear the LCD
+# lcd.clear()
+
+# # Cleanup GPIO
+# GPIO.cleanup()
+
+import wiringpi as wp
+from time import sleep
+
 LCD_RS = 11
 LCD_E = 10
 LCD_D4 = 6
@@ -10,25 +44,24 @@ LCD_D5 = 5
 LCD_D6 = 4
 LCD_D7 = 1
 
-# Setup GPIO mode
-GPIO.setmode(GPIO.BCM)
+# Initialize wiringPi
+wp.wiringPiSetup()
 
 # Initialize the LCD
-lcd = CharLCD(
-    numbering_mode=GPIO.BCM,
-    cols=16, rows=2,
-    pin_rs=LCD_RS, pin_e=LCD_E,
-    pins_data=[LCD_D4, LCD_D5, LCD_D6, LCD_D7]
-)
+lcd = wp.lcdInit(2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0)
 
-# Display text on the LCD
-lcd.write_string('HELLO WORLD')
+if lcd == -1:
+    print("lcd init failed!")
+    exit(1)
+
+# Set the cursor to the first position (0, 0)
+wp.lcdPosition(lcd, 0, 0)
+
+# Print the message to the LCD
+wp.lcdPuts(lcd, "HELLO WORLD")
 
 # Wait for user input
-input("Press any key to continue...")
+input("Press Enter to clear the LCD...")
 
 # Clear the LCD
-lcd.clear()
-
-# Cleanup GPIO
-GPIO.cleanup()
+wp.lcdClear(lcd)
