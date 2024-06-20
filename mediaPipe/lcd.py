c605cfc50d90d2420da72b14f4b6859cfd5fae80
JongKyeong Kim <oo7bangjk@naver.com>
2024-06-20 14:18:28 +0900

    .

--- mediaPipe/lcd.py
+++ mediaPipe/lcd.py
@@ -2,13 +2,13 @@ import RPi.GPIO as GPIO
 from RPLCD.gpio import CharLCD
 import time
 
-# Pin configuration using BCM numbering
-LCD_RS = 17  # BCM pin number for GPIO 11
-LCD_E = 22   # BCM pin number for GPIO 10
-LCD_D4 = 25  # BCM pin number for GPIO 6
-LCD_D5 = 24  # BCM pin number for GPIO 5
-LCD_D6 = 23  # BCM pin number for GPIO 4
-LCD_D7 = 18  # BCM pin number for GPIO 1
+# Pin configuration using BCM numbering (same pins as in your C code)
+LCD_RS = 11  # WiringPi 11 corresponds to BCM 17
+LCD_E = 10   # WiringPi 10 corresponds to BCM 27
+LCD_D4 = 6   # WiringPi 6 corresponds to BCM 22
+LCD_D5 = 5   # WiringPi 5 corresponds to BCM 24
+LCD_D6 = 4   # WiringPi 4 corresponds to BCM 23
+LCD_D7 = 1   # WiringPi 1 corresponds to BCM 18
 
 # Initialize GPIO
 GPIO.setwarnings(False)
@@ -34,6 +34,7 @@ lcd.clear()
 
 # Clean up GPIO
 GPIO.cleanup()
+
 
 
 
